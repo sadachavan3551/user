d48174240919fb0a223f4754c219c1d404b9e4c1
sadachavan3551 <sadanandchavan2005@gmail.com>
2026-05-14 17:19:25 +0530

    Add files via upload

---
+++ reversestack.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Stack {
+public:
+    char st[50];
+    int top;
+
+    Stack() {
+        top = -1;
+    }
+
+    void push(char ch) {
+        top++;
+        st[top] = ch;
+    }
+
+    char pop() {
+        char ch = st[top];
+        top--;
+        return ch;
+    }
+
+    int isEmpty() {
+        if (top == -1)
+            return 1;
+        else
+            return 0;
+    }
+};
+
+int main() {
+    Stack s;
+    string str;
+    string rev = "";
+
+    cout << "Enter string: ";
+    cin >> str;
+
+    for (int i = 0; i < str.length(); i++) {
+        s.push(str[i]);
+    }
+
+    while (!s.isEmpty()) {
+        rev = rev + s.pop();
+    }
+
+    cout << "Original string: " << str << endl;
+    cout << "Reversed string: " << rev << endl;
+
+    return 0;
+}
